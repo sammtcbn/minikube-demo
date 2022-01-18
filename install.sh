93a8f1380a6308480131c2c947590cde63b1c5df
sammtcbn <sammtcbn@gmail.com>
2022-01-19 00:34:41 +0800

    draft

--- install.sh
+++ install.sh
@@ -5,11 +5,20 @@ sudo apt -y install curl
 
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
+minikube version
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt -y update
+sudo apt -y install kubectl kubeadm
 
 sudo apt -y install docker.io
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo usermod -aG docker $USER
+docker --version
 newgrp docker
 
 minikube start --driver=docker
+
+minikube dashboard --url
